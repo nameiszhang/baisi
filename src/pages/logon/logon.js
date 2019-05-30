6c1660a273b17e27aff1c6dee91339aa86e570bd
zzx <2380859352@qq.com>
2019-05-30 19:09:05 +0800

    登录页

--- src/pages/logon/logon.js
+++ src/pages/logon/logon.js
@@ -7,6 +7,12 @@ export default class Logon extends Component {
     config = {
         navigationBarTitleText: '登录'
     }
+    constructor(){
+        this.state={
+            username:'',
+            password:''
+        }
+    }
 
     componentWillMount () { }
 
@@ -22,19 +28,59 @@ export default class Logon extends Component {
         return (
         <View className='logon'>
             <View>
-                <Input placeholder='输入用户名'/>
-                <Input placeholder='请输入密码'/>
+                <View>
+                    <Input onInput={(e)=>{
+                        this.setState({
+                            username: e.detail.value
+                        })
+                    }} placeholder='输入用户名'/>
+                    <Input onInput={(e)=>{
+                        this.setState({
+                            password: e.detail.value
+                        })
+                    }} placeholder='请输入密码' type='password'/>
+                </View>
+                <View className='btn'>
+                    {/* <Navigator url='/pages/mine/mine'> */}
+                        <Button type='warn' onClick={()=>{
+                            this.logon()
+                        }}>登录</Button>
+                    {/* </Navigator> */}
+                </View>
+                <View className='texts'>
+                    <Text>立即注册</Text>
+                    <Text>忘记密码</Text>
+                </View>
             </View>
-            <View>
-                <Navigator url='/pages/mine/mine'>
-                    <Button>登录</Button>
-                </Navigator>
-            </View>
-            <View>
-                <Text>立即注册</Text>
-                <Text>忘记密码</Text>
-            </View>
+            
         </View>
         )
+    }
+    logon=()=>{
+        let {username,password}=this.state
+        console.log(username,password)
+        
+        if (username!==''&& password!==''){
+            Taro.switchTab({
+            url: '/pages/mine/mine'
+            })
+
+        }else{
+            if (username === '' && password !== '') {
+                Taro.showModal({
+                    title: '请输入用户名'
+                })
+            } else if (username !== '' && password === '') {
+                Taro.showModal({
+                    title: '请输入密码'
+                })
+            } else if (username === '' && username === '') {
+                Taro.showModal({
+                    title: '请输入用户名和密码'
+                })
+            }
+        }
+        
+        
     }
 }
