36fcc49741f522335dd8b4ff94b3fff19425dbe6
zzx <2380859352@qq.com>
2019-05-30 16:24:02 +0800

    关注页

--- src/pages/attention/attention.js
+++ src/pages/attention/attention.js
@@ -21,10 +21,20 @@ export default class Attention extends Component {
     render () {
         return (
         <View className='attention'>
-            <Image src='http://localhost:3000/images/关注page.png'></Image>
-            <Navigator url='/pages/logon/logon'>
-                <Button>立即登录/注册</Button>
-            </Navigator>
+            <View>
+                <Image src='http://localhost:3000/images/关注page.png'></Image>
+            </View>
+            <View className='text'>
+                 <View>快快登录吧，关注百思最in牛人</View>
+                <View>好友动态让你过把瘾儿~</View>
+                <View>欧耶~~~</View>
+            </View>
+            <View>
+                <Navigator url='/pages/logon/logon'>
+                    <Button>立即登录/注册</Button>
+                </Navigator>
+            </View>
+            
         </View>
         )
     }
